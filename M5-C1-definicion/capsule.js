b84b87fb2e2c2758357ceb725d55a1952b6b1a98
unknown <ismaelocrdz@gmail.com>
2024-09-05 20:09:15 -0600

    Cambios en M5-C1 Pictures of You

---
+++ M5-C1-definicion/capsule.js
@@ -0,0 +1,4 @@
+const helloTag=document.querySelector("#hello")
+console.log(helloTag)
+const greetingTag=document.querySelector(".greeting")
+console.log(greetingTag)
---
+++ M5-C1-definicion/index.js
@@ -0,0 +1,4 @@
+const mainTitle=document.querySelector("#mainTitle")
+console.log(mainTitle)
+const h2Title=document.querySelector(".h2Title")
+console.log(h2Title)
